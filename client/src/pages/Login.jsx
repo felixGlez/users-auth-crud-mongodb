ce90cc1ca356ce02d81916497db5a7382b9485d9
felixGlez <felixgonzalezgarcia2010@gmail.com>
2024-06-26 20:47:56 +0200

    WIP

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,45 @@
+import { useContext, useState } from 'react';
 import { StyledContent } from './styles';
+import { loginRequest } from '../utils/api/auth.api';
+import { AuthContext } from '../contexts/AuthContext';
 
 const Login = () => {
+	const { userData, setUserData, loading } = useContext(AuthContext);
+	const [loginData, setLoginData] = useState({});
+
 	return (
 		<StyledContent>
 			<p>LOGIN</p>
-			<form>
-				<input type='text' placeholder='USERNAME' />
-				<input type='password' placeholder='PASSWORD' />
-				<button type='send' value='LOGIN'>
-					LOGIN
-				</button>
+			<form onSubmit={event => handleSubmit(event, loginData, setUserData)}>
+				<input
+					type='text'
+					placeholder='USERNAME'
+					onChange={event =>
+						setLoginData({ ...loginData, email: event.target.value })
+					}
+				/>
+				<input
+					type='password'
+					placeholder='PASSWORD'
+					onChange={event =>
+						setLoginData({ ...loginData, password: event.target.value })
+					}
+				/>
+				<button>LOGIN</button>
 			</form>
 		</StyledContent>
 	);
+};
+
+const handleSubmit = async (event, loginData, setUserData) => {
+	event.preventDefault();
+
+	try {
+		await loginRequest(loginData, setUserData);
+	} catch (error) {
+		console.error('Error de inicio de sesión', error);
+		// Puedes manejar el error aquí, por ejemplo, mostrar un mensaje de error
+	}
 };
 
 export default Login;
